dbd9038901cf95620a52eddd8ad95202453f428e
Owen Royall-Kahin <oroyallkahin@gmail.com>
2021-05-16 11:49:07 +0200

    prettier

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import {Meta} from "@storybook/react/types-6-0";
-import Button, { Props as ButtonProps } from './Button';
-import {Story} from "@storybook/react";
+import { Meta } from "@storybook/react/types-6-0";
+import Button, { Props as ButtonProps } from "./Button";
+import { Story } from "@storybook/react";
 
 export default {
-    title: 'Components/Button',
-    component: Button
+  title: "Components/Button",
+  component: Button,
 } as Meta;
 
-const Template: Story<ButtonProps> = (args) => <Button {...args}/>;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = { variant: 'primary', label: 'Click me!' }
+Primary.args = { variant: "primary", label: "Click me!" };
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require('path');
+const path = require("path");
 
-const LIBRARY_NAME = 'o-components';
+const LIBRARY_NAME = "o-components";
 
 module.exports = {
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "lib"),
+    filename: `${LIBRARY_NAME}.js`,
+    library: {
+      name: LIBRARY_NAME,
+      type: "umd",
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        path: path.resolve(__dirname, 'lib'),
-        filename: `${LIBRARY_NAME}.js`,
-        library: {
-            name: LIBRARY_NAME,
-            type: "umd"
-        },
-    },
-    externals: {
-        react: 'react'
-    }
+  },
+  externals: {
+    react: "react",
+  },
 };
